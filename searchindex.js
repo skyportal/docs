847c8042b3ea9bc1f0628b1cb5428e2e0d71e062
Travis CI User <travis@example.org>
2017-10-11 21:46:18 +0000

    Update docs after building Travis build 146 of skyportal/skyportal
    The docs were built from the branch 'master' against the commit
    8a5395d112835a9be70a6cbbede70f1df51f41c1.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/skyportal/skyportal/jobs/286739919.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --gh-pages-docs . --deploy-repo skyportal/docs

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index","install"],envversion:53,filenames:["index.rst","install.md"],objects:{},objnames:{},objtypes:{},terms:{docker:0,instal:1,setup:0,some:1,stuff:1,test:0},titles:["Skyportal","Setup"],titleterms:{docker:1,setup:1,skyport:0,test:1}})
